87e540d40a73587de7b50ba9e94c65b85e68c8e8
Muzamil Ahmad <66633369+Muzamil-07@users.noreply.github.com>
2024-08-19 23:31:00 +0500

    added script again

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Stack } from '@mui/material'
 import Menu from '../components/Menu'
 import Configurator from '../containers/Configurator'
 import { preloadTextures } from '../lib/utils'
 const Home = () => {
+  useEffect(() => {
+    preloadTextures()
+  }, [])
   return (
     <Stack>
       <Configurator />
